c3d7ce45c20768f2cecfed0d86db889b74a21f3e
Charles Cruse <charle9@vt.edu>
2024-04-27 01:18:39 -0400

    added User model

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log(error);
+});
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)})
+app.listen(PORT, () => {
+    // connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`)
+})
