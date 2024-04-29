0a9451617e5f63f813c4f854196554e96ddd168f
Charles Cruse <charle9@vt.edu>
2024-04-28 23:36:23 -0400

    connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGODB_URI)
 const app = express();
 
 const PORT = process.env.PORT || 5000;
